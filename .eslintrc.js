d294dd8e8f26b1fd3b372d55e9e3144f0e54e98b
shailesh kumar <smourya7425@gmail.com>
2024-10-25 09:03:05 +0530

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  extends: [
+    'prettier',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': [0, { properties: 'never' }],
+    'no-duplicate-imports': 'warn',
+  },
+};
